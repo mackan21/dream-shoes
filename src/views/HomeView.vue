e090c4de116b339a7b940b13115a139c841cbf1b
Marcus <mackan166@hotmail.com>
2025-02-26 14:49:53 +0100

    #15

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,13 @@
 
 <template>
   <main>
+    <section class="hero">
+      <img src="../assets/images/hero.webp" alt="" />
+      <div class="hero-content">
+        <h1>VÄLKOMMEN TILL DREAMSHOES</h1>
+        <RouterLink class="hero-button">ALLA SKOR</RouterLink>
+      </div>
+    </section>
     <section class="products">
       <div class="products-container">
         <div class="product-card">
@@ -34,6 +41,40 @@
 </template>
 
 <style scoped>
+/* Hero section */
+.hero {
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+}
+.hero img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.hero-content {
+  position: absolute;
+  top: 20%;
+  left: 2%;
+  color: white;
+  max-width: 600px;
+}
+h1 {
+  font-size: 4rem;
+  font-weight: bold;
+}
+.hero-button {
+  display: inline-block;
+  background: white;
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+  padding: 1rem 2rem;
+  border-radius: 50px;
+  border: 2px solid black;
+  margin-top: 1rem;
+}
+/* Bestseller section */
 .product-image {
   width: 275px;
 }
